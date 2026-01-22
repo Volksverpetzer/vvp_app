5ecf714b4a30a663c4d1f87f6420d846c8898562
veeck <gitkraken@veeck.de>
2026-01-22 10:40:55 +0100

    Add eslint plugin for absolute imports

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,5 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "*.{js,jsx,json,md,ts,tsx,yml,yaml}": "prettier --write",
+  "*.{js,jsx,json,md,mjs,ts,tsx,yml,yaml}": "prettier --write",
 };
