8090bd7fcbcd6858bbf77f4684f6e117431a9dbc
Veeck <github@veeck.de>
2026-02-04 22:11:30 +0100

    Pass more info down to ArticleSourceList (#27)
    Co-authored-by: veeck <gitkraken@veeck.de>

--- src/screens/Home/components/article/Header.tsx
+++ src/screens/Home/components/article/Header.tsx
@@ -34,6 +34,7 @@ interface HeaderProperties {
   article_title: string;
   article_link: string;
   date: string;
+  slug: string;
 }
 
 /**
@@ -42,13 +43,14 @@ interface HeaderProperties {
  * @returns
  */
 const Header = (properties: HeaderProperties) => {
+  const { article, article_image, article_link, article_title, date, slug } =
+    properties;
   const [visible, setVisible] = useState(false);
   const [imageLoaded, setImageLoaded] = useState(false);
   const [imageLoaded2, setImageLoaded2] = useState(false);
   const [urlCopied, setUrlCopied] = useState(false);
   const { width } = useWindowDimensions();
   const [height, setHeight] = useState(Math.round(0.5125 * width));
-  const { article_link } = properties;
   // Reference to the ViewShot component for image capture
   const reference = useRef<ViewShot>(null);
   const router = useRouter();
@@ -111,7 +113,6 @@ const Header = (properties: HeaderProperties) => {
     setHeight(Math.round(0.5125 * width));
   };
 
-  const { article, article_image, article_title, date } = properties;
   return (
     <>
       <MaterialIcons
@@ -183,7 +184,11 @@ const Header = (properties: HeaderProperties) => {
       </Text>
       <ArticleStats article_link={article_link} />
       <Space size={10} />
-      <ArticleSourceList article_link={article_link} />
+      <ArticleSourceList
+        article_link={article_link}
+        article_title={article_title}
+        slug={slug}
+      />
       <Space size={20} />
       <Modal visible={visible}>
         <ViewShot ref={reference} options={{ fileName: article_title }}>
