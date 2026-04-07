855dd1e78f14a9da0181510b8a958f335aa01b85
veeck <gitkraken@veeck.de>
2026-04-07 11:43:18 +0200

    Pass children to checkbox to increase toucbsize

--- src/components/design/Checkbox.tsx
+++ src/components/design/Checkbox.tsx
@@ -8,6 +8,7 @@ import { useAppColorScheme } from "#/hooks/useAppColorScheme";
 interface CheckboxProperties {
   checked: boolean;
   onChange: (checked: boolean) => void;
+  children?: React.ReactNode;
 }
 
 const Checkbox = (properties: CheckboxProperties) => {
@@ -30,10 +31,16 @@ const Checkbox = (properties: CheckboxProperties) => {
     <Pressable
       accessibilityRole="checkbox"
       accessibilityState={{ checked }}
-      style={[styles.checkboxBase, style, checkedStyle]}
+      style={[styles.row]}
       onPress={onPress}
     >
-      {checked && <CheckboxIcon size={24} color="white" />}
+      <Pressable
+        style={[styles.checkboxBase, style, checkedStyle]}
+        onPress={onPress}
+      >
+        {checked && <CheckboxIcon size={24} color="white" />}
+      </Pressable>
+      {properties.children}
     </Pressable>
   );
 };
@@ -45,6 +52,11 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     height: 28,
     width: 28,
+  },
+  row: {
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 20,
   },
 });
 
