233bea5dea77390e454c1aba36b5d39952a8e91c
veeck <gitkraken@veeck.de>
2026-04-09 12:59:30 +0200

    Simplify predicates

--- src/helpers/utils/typePredicates.ts
+++ src/helpers/utils/typePredicates.ts
@@ -1,19 +1,16 @@
-type UnknownRecord = Record<string, unknown>;
-
-export const isObjectRecord = (value: unknown): value is UnknownRecord =>
+export const isObjectRecord = (value: unknown) =>
   typeof value === "object" && value !== null;
 
 export const hasStringProperty = <TKey extends string>(
   value: unknown,
   key: TKey,
 ): value is Record<TKey, string> =>
-  isObjectRecord(value) && typeof value[key] === "string";
+  isObjectRecord(value) &&
+  typeof (value as Record<string, unknown>)[key] === "string";
 
-export const hasUri = (value: unknown): value is { uri: string } =>
-  hasStringProperty(value, "uri");
+export const hasUri = (value: unknown) => hasStringProperty(value, "uri");
 
-export const hasCreatedAt = (value: unknown): value is { created_at: string } =>
+export const hasCreatedAt = (value: unknown) =>
   hasStringProperty(value, "created_at");
 
-export const hasText = (value: unknown): value is { text: string } =>
-  hasStringProperty(value, "text");
+export const hasText = (value: unknown) => hasStringProperty(value, "text");
