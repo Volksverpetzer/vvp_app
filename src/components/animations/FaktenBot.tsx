bbb28199e9f11148f6ae0e5f1d3516e2c9af906f
veeck <gitkraken@veeck.de>
2026-04-20 15:16:02 +0200

    Split into native and default

--- src/components/animations/FaktenBot.tsx
+++ src/components/animations/FaktenBot.tsx
@@ -1,8 +1,3 @@
-import { RiveView, useRive, useRiveFile } from "@rive-app/react-native";
-import Constants from "expo-constants";
-import { useEffect } from "react";
-import { Platform } from "react-native";
-
 export type FaktenBotReaction = 0 | 5 | 10;
 
 interface FaktenBotProperties {
@@ -10,50 +5,8 @@ interface FaktenBotProperties {
   search?: boolean;
 }
 
-const FaktenBotRive = ({ reaction, search }: FaktenBotProperties) => {
-  const { riveFile } = useRiveFile("faktenbot5");
-  const { riveViewRef, setHybridRef } = useRive();
-
-  useEffect(() => {
-    if (reaction === undefined || !riveViewRef) return;
-    riveViewRef.setNumberInputValue("Reaktion", reaction);
-    riveViewRef.triggerInput("ResultIn");
-  }, [reaction, riveViewRef]);
-
-  useEffect(() => {
-    if (search === undefined || !riveViewRef) return;
-    riveViewRef.setBooleanInputValue("Suche", search);
-  }, [search, riveViewRef]);
-
-  if (!riveFile) return null;
-
-  return (
-    <RiveView
-      autoPlay={true}
-      file={riveFile}
-      hybridRef={setHybridRef}
-      stateMachineName="State Machine Search"
-      style={{
-        maxWidth: 150,
-        maxHeight: 150,
-        width: "100%",
-        height: "100%",
-      }}
-    />
-  );
-};
-
-/**
- * Animated FaktenBot component that displays different reactions based on search state
- * @param reaction - The reaction state (0, 5, or 10)
- * @param search - Whether the component is in search mode
- */
-const FaktenBot = ({ reaction, search }: FaktenBotProperties) => {
-  // Don't render in Web or Expo Go
-  if (Platform.OS === "web" || Constants.executionEnvironment === "storeClient")
-    return null;
-
-  return <FaktenBotRive reaction={reaction} search={search} />;
+const FaktenBot = (_props: FaktenBotProperties) => {
+  return null;
 };
 
 export default FaktenBot;
