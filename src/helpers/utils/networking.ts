a8edff00be964f84fdb9d72160153b847a0572c0
veeck <gitkraken@veeck.de>
2026-05-27 14:39:27 +0200

    Use expo/fetch

--- src/helpers/utils/networking.ts
+++ src/helpers/utils/networking.ts
@@ -1,6 +1,7 @@
 import * as Application from "expo-application";
 import Constants from "expo-constants";
 import * as Device from "expo-device";
+import { fetch } from "expo/fetch";
 import { Platform } from "react-native";
 
 import type { HttpsUrl } from "#/types";
