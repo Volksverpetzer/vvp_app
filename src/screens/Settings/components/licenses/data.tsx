c238cffe70be9309a4cde7ff22172a6c86b541bc
veeck <gitkraken@veeck.de>
2026-07-08 08:27:41 +0200

    Bump more deps

--- src/screens/Settings/components/licenses/data.tsx
+++ src/screens/Settings/components/licenses/data.tsx
@@ -5,7 +5,7 @@ export default {
     licenseUrl:
       "https://github.com/algolia/algoliasearch-client-javascript/raw/HEAD/LICENSE",
   },
-  "@atproto/api@0.20.25": {
+  "@atproto/api@0.20.26": {
     licenses: "MIT",
     repository: "https://github.com/bluesky-social/atproto",
     licenseUrl:
@@ -63,7 +63,7 @@ export default {
     licenseUrl:
       "https://github.com/oblador/react-native-vector-icons/raw/HEAD/LICENSE",
   },
-  "@rive-app/react-native@0.4.13": {
+  "@rive-app/react-native@0.4.14": {
     licenses: "MIT",
     repository: "https://github.com/rive-app/rive-nitro-react-native",
     licenseUrl:
