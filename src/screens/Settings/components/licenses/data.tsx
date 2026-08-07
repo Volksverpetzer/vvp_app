f2c41bceaf15efaece02e5c62fd4a89ab47f54bf
veeck <gitkraken@veeck.de>
2026-08-07 16:59:57 +0200

    Bump deps

--- src/screens/Settings/components/licenses/data.tsx
+++ src/screens/Settings/components/licenses/data.tsx
@@ -5,7 +5,7 @@ export default {
     licenseUrl:
       "https://github.com/algolia/algoliasearch-client-javascript/raw/HEAD/LICENSE",
   },
-  "@atproto/api@0.20.37": {
+  "@atproto/api@0.20.38": {
     licenses: "MIT",
     repository: "https://github.com/bluesky-social/atproto",
     licenseUrl:
